416f020e7a9a1df57feff7f05ac83257114ea335
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-11-28 19:25:01 +0000

    Adjusted ManualJumpMovement so it is able to make a default move every frame, without getting stuck jumping all the time (etc.)

--- src/ch/idsia/agents/controllers/kbarrett/ManualJumpMovement.java
+++ src/ch/idsia/agents/controllers/kbarrett/ManualJumpMovement.java
@@ -19,53 +19,58 @@ public class ManualJumpMovement extends Movement {
 			if(location == null)
 			{
 				defaultMove();
-				return;
 			}
-			//If the location provided is invalid, print out an error
-			if(location.length != 2)
+			else
 			{
-				System.err.println("Cannot move to this location.");
-				return;
-			}
-			
-			//Deciding whether to make a vertical movement
-				//If the location provided is higher on the screen than Mario's current position, he needs to move higher.
-				if(location[0] < marioMapLoc[0])
+				//If the location provided is invalid, print out an error
+				if(location.length != 2)
 				{
-					if(canJumpNextGo)
-					{
-						jump();
-						jumping = true;
-					}
+					System.err.println("Cannot move to this location.");
+					return;
 				}
-			
-			//Deciding whether to make a horizontal movement
-				//If the required location is further right on the screen than Mario, move him right.
-				if(location[1] > marioMapLoc[1])
-				{
-					goRight();
-				}
-				//If it is further left, move him left.
-				else if(location[1] < marioMapLoc[1])
-				{
-					goLeft();
-				}
-				//If it is level with where he is, don't move - this could be the case if jumping for a coin.
 				
-				canJumpNextGo = true;
+				//Deciding whether to make a vertical movement
+					//If the location provided is higher on the screen than Mario's current position, he needs to move higher.
+					if(location[0] < marioMapLoc[0])
+					{
+						if(canJumpNextGo)
+						{
+							jump();
+						}
+					}
+				
+				//Deciding whether to make a horizontal movement
+					//If the required location is further right on the screen than Mario, move him right.
+					if(location[1] > marioMapLoc[1])
+					{
+						goRight();
+					}
+					//If it is further left, move him left.
+					else if(location[1] < marioMapLoc[1])
+					{
+						goLeft();
+					}
+					//If it is level with where he is, don't move - this could be the case if jumping for a coin.
+			}
+				
+			canJumpNextGo = true;
 	}
 
 	@Override
 	protected void defaultMove()
 	{
 		goRight();
-		jump();
+		if(canJumpNextGo)
+		{
+			jump();
+		}
 	}
 
 	@Override
 	protected void jump()
 	{
 		actions[Mario.KEY_JUMP] = true;
+		jumping = true;
 	}
 	@Override
 	public void land()
